c394886ec1f2a30aec16541c566312cd154f147b
bram aribowo <donyaribowo@gmail.com>
2018-10-24 16:33:05 +0700

    receive and save message to db

--- server.js
+++ server.js
@@ -16,14 +16,45 @@ else{
   var rabbiturl = "amqp://localhost";
 }
 
+// mongoose.connect('mongodb://localhost/restful-prototype');
+mongoose.connect(mongourl);
+
+var AuditSchema = mongoose.Schema({
+  user: String,
+  action: String,
+  time : { type : Date, default: Date.now() }
+});
+var Audit = restful.model('Audit', AuditSchema);
+Audit.methods(['get', 'put','post','delete']);
+
+Audit.route('removeall', ['delete'], function(req, res, next) {
+  Audit.remove({}, function (err) {
+        if (err) {
+          console.log(err);
+        } 
+    });
+    res.send("records cleared!");
+});
+
+function saveAudit(msg) {
+    var audit = new Audit({user:'anonymous', action: msg, time: Date.now()});
+    audit.save(function (err) {
+        if (err) {
+          console.log(err);
+        } 
+        // else saved!
+        return;
+    });
+}
+
 amqp.connect(rabbiturl, function(err, conn) {
   conn.createChannel(function(err, ch) {
     var q = 'audit';
 
     ch.assertQueue(q, {durable: false});
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());
+      saveAudit(msg.content.toString());
     }, {noAck: true});
   });
 });
@@ -36,17 +67,8 @@ app.use(methodOverride());
 app.use(bodyParser.json()); 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-// mongoose.connect('mongodb://localhost/restful-prototype');
-mongoose.connect(mongourl);
-
-var AuditSchema = mongoose.Schema({
-	user: String,
-	action: String,
-	time : { type : Date, default: Date.now }
-});
-var Audits = restful.model('audits', AuditSchema);
-Audits.methods(['get', 'put','post','delete']);
-Audits.register(app, '/api/audits');
+//register api
+Audit.register(app, '/api/audits');
 
 app.listen(port, host);
-console.log("Server is on air, port 3001");
+console.log("Server is on air, port " + port);
